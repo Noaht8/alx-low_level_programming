46d5928425f5abe72c6f49587427c24f522b4efd
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-08 06:35:10 -0700

    Size of Variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'program that prints the size of various types'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int ll;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+	return (0);
+}
