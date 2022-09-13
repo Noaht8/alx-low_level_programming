21521b9b88510b6c8738570c8bee73c97716c212
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-13 19:31:05 +0000

    Fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define LARGEST 10000000000
 
 /**
  * main - main function to display first 98 fibonacci numbers
@@ -7,25 +8,37 @@
  */
 int main(void)
 {
-	int i;
 	unsigned long int t1 = 0;
-	unsigned long int t2 = 1;
-	unsigned long int next;
+	unsigned long int b1 = 1;
+	unsigned long int t2 = 0;
+	unsigned long int b2 = 2;
+	unsigned long int h1, h2, h3;
+	int i;
 
-	for (i = 1; i <= 97; i++)
+	printf("%lu, %lu, ", b1, b2);
+	for (i = 2; i < 98; i++)
 	{
-		next = t1 + t2;
-
-		if (i < 97)
+		if (b1 + b2 > LARGEST || t2 > 0 || t1 > 0)
 		{
-			printf("%lu, ", next);
+			h1 = (b1 + b2) / LARGEST;
+			h2 = (b1 + b2) % LARGEST;
+			h3 = t1 + t2 + h1;
 			t1 = t2;
-			t2 = next;
+			t2 = h3;
+			b1 = b2;
+			b2 = h2;
+			printf("%lu%010lu", t2, b2);
 		}
 		else
 		{
-			printf("%lu\n", next);
+			h2 = b1 + b2;
+			b1 = b2;
+			b2 = h2;
+			printf("%lu", b2);
 		}
+		if (i != 97)
+			printf(", ");
 	}
+	printf("\n");
 	return (0);
 }
