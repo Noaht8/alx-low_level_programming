de7bbe4744ad3a7075ae4b9c6c5a7de126a67915
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-13 12:30:35 +0000

    Lowercase Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print lowercase
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
