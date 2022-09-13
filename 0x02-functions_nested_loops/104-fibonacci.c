5c24b95af9a617b1351e4b39659d7c1183edaf26
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-13 18:58:47 +0000

    Fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,11 +12,11 @@ int main(void)
 	unsigned long int t2 = 1;
 	unsigned long int next;
 
-	for (i = 1; i <= 98; i++)
+	for (i = 1; i <= 97; i++)
 	{
 		next = t1 + t2;
 
-		if (i < 98)
+		if (i < 97)
 		{
 			printf("%lu, ", next);
 			t1 = t2;
