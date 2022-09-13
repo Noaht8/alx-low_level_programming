f0fe5b653b2379ea6ad765a1771fc1747c0e981b
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-13 13:54:49 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - function to identify sign of number
+ * @n: input for function
+ *
+ * Return: 1 (> zero), 0 (equal to zero) and -1 (< zero)
+ */
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
