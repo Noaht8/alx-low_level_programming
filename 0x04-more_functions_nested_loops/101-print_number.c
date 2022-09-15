19a236ed576bb5c3c10dfdea4e9887018424032e
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-15 11:48:08 +0000

    Print Number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Function to print any number
+ * @n: input parameter number
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int a = 0;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+	}
+
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
