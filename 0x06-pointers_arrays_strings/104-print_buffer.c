d63914b44a174d87de0498684b1c495ca16c58e0
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-22 22:23:53 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,44 +9,41 @@
  */
 void print_buffer(char *b, int size)
 {
-	int a, c;
+	int o, j, i;
+
+	o = 0;
 
 	if (size <= 0)
-		printf("\n");
-	else
 	{
-		for (a = 0; a < size; a += 10)
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
 		{
-			printf("%08x: ", a);
-			c = 0;
-			while (c < 10)
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
 			{
-				if (c % 2 == 0 && c != 0)
-				{
-					printf(" ");
-				}
-				if ((c + a) > size - 1)
-				{
-					printf(" ");
-				}
-				else
-				{
-					printf("%.2x", b[a + c]);
-				}
-				c++;
+				printf(" ");
 			}
-			printf(" ");
-			c = 0;
-			while (c < 10)
-			{
-				if ((c + a) > size - 1)
-					break;
-				if (b[c + a] <= 31 || b[c + 1] >= '~')
-					b[c + a] = '.';
-				putchar (b[c + a]);
-				c++;
-			}
-			printf("\n");
 		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
