dc4407da28b8604f77e3d1f722438e77f6691a2c
Noah Tsegay <noahtsegay8@gmail.com>
2022-09-22 15:50:29 +0000

    Print Integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints a number
+ * @n: input Integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -n;
+	}
+	if (a / 10 != 0)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + '0');
+}
