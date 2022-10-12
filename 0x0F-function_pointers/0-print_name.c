ef880a92545ca4d2ee0741bdb76a33af7369bc3f
Noah Tsegay <noahtsegay8@gmail.com>
2022-10-12 09:12:58 +0000

    Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,12 @@
  * print_name - function that prints a name
  * @name: Name to be Printed
  * @f: A pointer to a function that accepts a string and
- * returns nothing
- * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
+	{
 		return;
+	}
 	f(name);
 }
