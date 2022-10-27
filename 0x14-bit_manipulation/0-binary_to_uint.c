e44d195d83519ea2e0bd7a13be1de99ed99a5906
Noah Tsegay <noahtsegay8@gmail.com>
2022-10-27 10:44:22 +0000

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+#include <string.h>
 
 /**
  * binary_to_uint - function that converts a binary number
@@ -10,16 +10,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int v = 0;
-	int i = 0;
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
 
-	if (b == NULL)
-		return (0);
-	while (b[i] == '0' || b[i] == '1')
+	len = strlen(b);
+
+	for (c = len - 1; c >= 0; c--)
 	{
-		v <<= 1;
-		v += b[i] - '0';
-		i++;
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+			i += k;
+		k *= 2;
 	}
-	return (v);
+	return (i);
 }
