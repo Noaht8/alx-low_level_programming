995c8cb23b6337ca0d7dcafa1aa5f9aedd54ffa9
Noah Tsegay <noahtsegay8@gmail.com>
2022-12-12 22:32:30 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Noaht8/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winrandom.so
-export LD_PRELOAD=/tmp/winrandom.so
+wget -P /tmp https://github.com/Noaht8/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winjack.so
+export LD_PRELOAD=/tmp/winjack.so
