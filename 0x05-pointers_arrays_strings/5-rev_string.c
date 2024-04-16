0a4ae014193c3a62256c0c607f37d2bb81ebf3f1
Noah Tsegay <noahtsegay8@gmail.com>
2024-04-16 11:51:02 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,30 +9,15 @@
  */
 void rev_string(char *s)
 {
-	int a, b;
-	int c = 0;
-	char revS[1000];
-
-	for (a = 0; 1; a++)
-	{
-		if (s[a] == 0)
-		{
-			break;
-		}
-		c++;
-	}
-	a = 0;
-	for (b = c - 1; b >= 0; b--)
-	{
-		revS[a] = s[b];
-		a++;
-	}
-	for (a = 0; 1; a++)
-	{
-		if (revS[a] == 0)
-		{
-			break;
-		}
-		s[a] = revS[a];
-	}
+    char str[1000] = "";
+    int i = strlen(s) - 1;
+    int j = 0;
+    while (s[i] >= 1)
+    {
+        str[j] = s[i];
+        i--;
+        j++;
+    }
+    str[j] = '\0';
+    strcpy(s, str);
 }
