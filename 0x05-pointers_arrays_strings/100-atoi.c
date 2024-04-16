f7abca0d258fe5922ea6cbd497c04d773989ed25
Noah Tsegay <noahtsegay8@gmail.com>
2024-04-16 16:15:46 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,33 +7,37 @@
  */
 int _atoi(char *s)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int l = 0;
-	int f = 0;
-	int d = 0;
 
-	while (s[l] != '\0')
-		l++;
-	while (a < l && f == 0)
-	{
-		if (s[a] == '-')
-			++b;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			d = s[a] - '0';
-			if (b % 2)
-				d = -d;
-			c = c * 10 + d;
-			f = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			f = 0;
-		}
-		a++;
-	}
-	if (f == 0)
-		return (0);
-	return (c);
+  int i = 0;
+  int j = 0;
+  int result = 0;
+  int sign = 1;
+  while (s[i] != '\0')
+  {
+
+    if (s[i] == '-')
+    {
+      sign *= -1;
+      i++;
+    }
+    else if (s[i] == '+')
+    {
+      i++;
+    }
+
+    if (s[i] >= '0' && s[i] <= '9')
+    {
+      result = result * 10 + s[i] - '0';
+      if (s[i + 1] < 48 || s[i + 1] > 57)
+      {
+        break;
+      }
+    }
+
+    i++;
+  }
+
+  int num = result * sign;
+
+  return num;
 }
